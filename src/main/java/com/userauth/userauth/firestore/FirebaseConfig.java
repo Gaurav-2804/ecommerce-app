da7ee5b4b9e79d12ed53fd9c07b0f6bf5e92b541
Gaurav-2804 <74343269+Gaurav-2804@users.noreply.github.com>
2024-01-03 15:45:14 +0530

    Update FirebaseConfig.java

--- src/main/java/com/userauth/userauth/firestore/FirebaseConfig.java
+++ src/main/java/com/userauth/userauth/firestore/FirebaseConfig.java
@@ -1,10 +1,5 @@
 package com.userauth.userauth.firestore;
 
-import java.io.FileInputStream;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.FirebaseApp;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
