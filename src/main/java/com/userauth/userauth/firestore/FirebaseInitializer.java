1bd351e106f9c2476e837e0484297904a48c97a5
Gaurav-2804 <74343269+Gaurav-2804@users.noreply.github.com>
2024-01-04 17:36:41 +0530

    Update FirebaseInitializer.java

--- src/main/java/com/userauth/userauth/firestore/FirebaseInitializer.java
+++ src/main/java/com/userauth/userauth/firestore/FirebaseInitializer.java
@@ -1,51 +1,51 @@
-package com.userauth.userauth.firestore;
+// package com.userauth.userauth.firestore;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+// import java.io.FileInputStream;
+// import java.io.IOException;
+// import java.io.InputStream;
 
-import javax.annotation.PostConstruct;
+// import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.core.io.Resource;
+// import org.springframework.core.io.ResourceLoader;
+// import org.springframework.stereotype.Service;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+// import com.google.auth.oauth2.GoogleCredentials;
+// import com.google.firebase.FirebaseApp;
+// import com.google.firebase.FirebaseOptions;
 
-@Service
-public class FirebaseInitializer {
-    @Autowired
-    ResourceLoader resourceLoader;
+// @Service
+// public class FirebaseInitializer {
+//     @Autowired
+//     ResourceLoader resourceLoader;
     
-    @PostConstruct
-    public void initialize() throws IOException {
-        // String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+//     @PostConstruct
+//     public void initialize() throws IOException {
+//         // String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
 
-        Resource resource = resourceLoader.getResource("classpath:service_account_pk.json");
+//         Resource resource = resourceLoader.getResource("classpath:service_account_pk.json");
 
-        // FileInputStream serviceAccount = new FileInputStream("./service_account_pk.json");
-        // FileInputStream serviceAccount;
+//         // FileInputStream serviceAccount = new FileInputStream("./service_account_pk.json");
+//         // FileInputStream serviceAccount;
 
-        InputStream inputStream = resource.getInputStream();
+//         InputStream inputStream = resource.getInputStream();
 
-        // if (credentialsPath != null) {
-        //     serviceAccount = new FileInputStream(credentialsPath);
-        // } else {
-        //     serviceAccount = new FileInputStream("./service_account_pk.json");
-        // }
+//         // if (credentialsPath != null) {
+//         //     serviceAccount = new FileInputStream(credentialsPath);
+//         // } else {
+//         //     serviceAccount = new FileInputStream("./service_account_pk.json");
+//         // }
 
-        GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);   
-        FirebaseOptions options = FirebaseOptions
-            .builder()
-            .setCredentials(credentials)
-            .setStorageBucket("e-com-services-f8918.appspot.com")
-            .build();
+//         GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);   
+//         FirebaseOptions options = FirebaseOptions
+//             .builder()
+//             .setCredentials(credentials)
+//             .setStorageBucket("e-com-services-f8918.appspot.com")
+//             .build();
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
-        }
-    }
-}
+//         if (FirebaseApp.getApps().isEmpty()) {
+//             FirebaseApp.initializeApp(options);
+//         }
+//     }
+// }
