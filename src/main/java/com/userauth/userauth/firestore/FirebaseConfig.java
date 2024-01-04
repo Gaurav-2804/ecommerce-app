f8a93cc412b3d4e4e8834a7f2b74a5d84dc26e13
Gaurav-2804 <74343269+Gaurav-2804@users.noreply.github.com>
2024-01-04 17:37:20 +0530

    Update FirebaseConfig.java

--- src/main/java/com/userauth/userauth/firestore/FirebaseConfig.java
+++ src/main/java/com/userauth/userauth/firestore/FirebaseConfig.java
@@ -1,15 +1,37 @@
 package com.userauth.userauth.firestore;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
 @Configuration
 public class FirebaseConfig {
     @Bean
-    public Firestore getDb() {
+    public Firestore getDb() throws IOException {
+        initializeFirebaseApp();
         return FirestoreClient.getFirestore();
+    }   
+
+    private void initializeFirebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("./service_account_pk.json");
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .setStorageBucket("e-com-services-f8918.appspot.com")
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
     }
 }
