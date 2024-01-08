467d170fa09a393fa4fbd20a4c9df3d96d14e72b
Gaurav-2804 <74343269+Gaurav-2804@users.noreply.github.com>
2024-01-08 11:07:25 +0530

    Update FirebaseConfig.java

--- src/main/java/com/userauth/userauth/firestore/FirebaseConfig.java
+++ src/main/java/com/userauth/userauth/firestore/FirebaseConfig.java
@@ -1,14 +1,14 @@
 package com.userauth.userauth.firestore;
 
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
@@ -22,9 +22,13 @@ public class FirebaseConfig {
     }   
 
     private void initializeFirebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("./service_account_pk.json");
 
-        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+        InputStream serviceAccountStream = getClass().getResourceAsStream("/service_account_pk.json");
+        if (serviceAccountStream == null) {
+            throw new FileNotFoundException("Service account JSON file not found in the classpath");
+        }
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccountStream);
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(credentials)
                 .setStorageBucket("e-com-services-f8918.appspot.com")
